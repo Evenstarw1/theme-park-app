a495ec4455ad054c7351d7069d9be43ce91da8f9
Laura Corral Lopez <lauracorrallopez@iMac-de-Laura.local>
2024-12-02 12:51:58 +0100

    feat(sidenav.*): Added config to share sidenav depends on login

--- src/app/Shared/Components/sidenav/sidenav.component.ts
+++ src/app/Shared/Components/sidenav/sidenav.component.ts
@@ -3,6 +3,9 @@ import { MatSidenav } from '@angular/material/sidenav';
 import { SidenavService } from 'src/app/Shared/Services/sidenav.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducers';
+import * as AuthAction from 'src/app/Auth/actions';
 
 @Component({
   selector: 'app-sidenav',
@@ -12,23 +15,31 @@ import { Router } from '@angular/router';
 export class SidenavComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() sidenavOpen: boolean = false;
   sidenavType: 'menu' | 'account' | null = null;
+  showAuthSection: boolean = false;
+  showNoAuthSection: boolean = true;
+
   private sidenavTypeSubscription!: Subscription;
   private sidenavOpenSubscription!: Subscription;
+  private authSubscription!: Subscription;
 
   @ViewChild('sidenav') sidenav!: MatSidenav;
 
-  constructor(private router: Router, private sidenavService: SidenavService) { }
+  constructor(private router: Router, private sidenavService: SidenavService, private store: Store<AppState>) { }
 
   ngOnInit(): void {
     this.sidenavTypeSubscription = this.sidenavService.sidenavType$.subscribe((type: 'menu' | 'account' | null) => {
       this.sidenavType = type;
     });
+
+    this.authSubscription = this.store.select('auth').subscribe((auth) => {
+      this.showAuthSection = !!auth.credentials?.access_token;
+      this.showNoAuthSection = !this.showAuthSection;
+    });
   }
 
   ngAfterViewInit(): void {
-    // Mueve la suscripción que maneja open/close a `ngAfterViewInit` para asegurar que sidenav esté disponible
     this.sidenavOpenSubscription = this.sidenavService.sidenavOpen$.subscribe(isOpen => {
-      if (this.sidenav) {  // Asegúrate de que sidenav está definido
+      if (this.sidenav) {
         if (isOpen) {
           this.sidenav.open();
         } else {
@@ -45,8 +56,12 @@ export class SidenavComponent implements OnInit, OnDestroy, AfterViewInit {
     if (this.sidenavOpenSubscription) {
       this.sidenavOpenSubscription.unsubscribe();
     }
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
   }
 
+  // Account navigation
   login(): void {
     this.router.navigateByUrl('login');
     this.sidenavService.closeSidenav(); 
@@ -57,6 +72,18 @@ export class SidenavComponent implements OnInit, OnDestroy, AfterViewInit {
     this.sidenavService.closeSidenav();
   }
 
+  profile(): void {
+    this.router.navigateByUrl('profile');
+    this.sidenavService.closeSidenav();
+  }
+
+  logout(): void {
+    this.store.dispatch(AuthAction.logout());
+    this.router.navigateByUrl('home');
+    this.sidenavService.closeSidenav();
+  }
+
+  // Menu navigation
   home(): void {
     this.router.navigateByUrl('home');
     this.sidenavService.closeSidenav();
